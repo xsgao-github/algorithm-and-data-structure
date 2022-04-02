fa95fd2fc75936b0ef524f9c0b6a563fed4de318
Song X. Gao <39278329+xsgao-github@users.noreply.github.com>
2022-04-02 03:06:45 -0400

    leetcode 138

---
+++ java-impl/app/src/main/java/ads/java/leetcode/LC0138CopyListWithRandomPointer.java
@@ -0,0 +1,63 @@
+package ads.java.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC0138CopyListWithRandomPointer {
+
+	public static class Node {
+		int val;
+		Node next;
+		Node random;
+
+		public Node(int val) {
+			this.val = val;
+			this.next = null;
+			this.random = null;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append(String.format("[%d, %s] > ", val, random == null ? "" : random.val + ""));
+			sb.append(next == null ? "null" : next.toString());
+			return sb.toString();
+		}
+	}
+
+	public Node copyRandomList(Node head) {
+		if (head == null) {
+			return null;
+		}
+
+		Node h1 = head;
+		Node h2 = new Node(h1.val);
+
+		// map of corresponding node in two lists
+		Map<Node, Node> map = new HashMap<>();
+
+		// copy
+		Node n1 = h1;
+		Node n2 = h2;
+		map.put(n1, n2);
+		while (n1.next != null) {
+			n2.next = new Node(n1.next.val);
+			n1 = n1.next;
+			n2 = n2.next;
+			map.put(n1, n2);
+		}
+
+		// set random pointer
+		n1 = h1;
+		n2 = h2;
+		while (n1 != null) {
+			if (n1.random != null) {
+				n2.random = map.get(n1.random);
+			}
+			n1 = n1.next;
+			n2 = n2.next;
+		}
+
+		return h2;
+	}
+}
---
+++ java-impl/app/src/test/java/ads/java/leetcode/LC0138CopyListWithRandomPointerTest.java
@@ -0,0 +1,38 @@
+package ads.java.leetcode;
+
+import ads.java.leetcode.LC0138CopyListWithRandomPointer.Node;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LC0138CopyListWithRandomPointerTest {
+
+	@Test
+	void testCopyRandomList() {
+		LC0138CopyListWithRandomPointer obj = new LC0138CopyListWithRandomPointer();
+
+		//[[7,null],[13,0],[11,4],[10,2],[1,0]]
+		Node n7 = new Node(7);
+		Node n13 = new Node(13);
+		Node n11 = new Node(11);
+		Node n10 = new Node(10);
+		Node n1 = new Node(1);
+
+		n7.next = n13;
+		n13.next = n11;
+		n11.next = n10;
+		n10.next = n1;
+		n1.next = null;
+
+		n7.random = null;
+		n13.random = n7;
+		n11.random = n1;
+		n10.random = n11;
+		n1.random = n7;
+		
+		assertEquals(n7.toString(), obj.copyRandomList(n7).toString());
+		
+		assertNull(obj.copyRandomList(null));
+	}
+
+}
