5792cd5172047aee5cc4f87fa417f374af21b2ec
Song X. Gao <39278329+xsgao-github@users.noreply.github.com>
2022-04-03 17:46:48 -0400

    leetcode 371

---
+++ java-impl/app/src/test/java/ads/java/leetcode/LC0371SumOfTwoIntegersTest.java
@@ -0,0 +1,18 @@
+package ads.java.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LC0371SumOfTwoIntegersTest {
+
+	@Test
+	void testGetSum() {
+		LC0371SumOfTwoIntegers obj = new LC0371SumOfTwoIntegers();
+//		assertEquals(10, obj.getSum(5, 5));
+//		assertEquals(0, obj.getSum(5, -5));
+//		assertEquals(1, obj.getSum(5, -4));
+		assertEquals(-1, obj.getSum(4, -5));
+	}
+
+}
