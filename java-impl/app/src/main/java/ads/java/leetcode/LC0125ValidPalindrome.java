170d645ba8d8b0371c7211c1de8c2e16566c59e3
Song X. Gao <39278329+xsgao-github@users.noreply.github.com>
2022-04-29 09:45:41 -0400

    add LeetCode0125

---
+++ java-impl/app/src/main/java/ads/java/leetcode/LC0125ValidPalindrome.java
@@ -0,0 +1,33 @@
+package ads.java.leetcode;
+
+public class LC0125ValidPalindrome {
+	public boolean isPalindrome(String str) {
+		// remove non-alphanumeric characters & convert all upper cases to lower cases
+		StringBuilder sb = new StringBuilder();
+		for (char chr : str.toCharArray()) {
+			if (chr >= '0' && chr <= '9') {
+				sb.append(chr);
+			} else if (chr >= 'a' && chr <= 'z') {
+				sb.append(chr);
+			} else if (chr >= 'A' && chr <= 'Z') {
+				sb.append((char) (chr + ('a' - 'A')));
+			} else {
+				// remove
+			}
+		}
+
+		// empty or only one char
+		if (sb.length() <= 1) {
+			return true;
+		}
+
+		// from left to the middle 
+		int len = sb.length();
+		for (int i = 0; i < len / 2; i++) {
+			if (sb.charAt(i) != sb.charAt(len - i - 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
