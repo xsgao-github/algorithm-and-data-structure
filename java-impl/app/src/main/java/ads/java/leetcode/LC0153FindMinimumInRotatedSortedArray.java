66ab43932e9579a16dbcae35638d3dd89981e273
Song X. Gao <39278329+xsgao-github@users.noreply.github.com>
2022-04-30 21:20:04 -0400

    add LeetCode 0153

---
+++ java-impl/app/src/main/java/ads/java/leetcode/LC0153FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,30 @@
+package ads.java.leetcode;
+
+public class LC0153FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+    	if (nums.length == 1) {
+    		// only one number
+    		return nums[0];
+    	}
+
+    	if (nums[0] < nums[nums.length - 1]) {
+    		// not rotated
+    		return nums[0];
+    	}
+
+    	// [4,5,6,7,0,1,2,3]
+    	int left = 1;
+    	int right = nums.length - 1;
+    	while (left < right) {
+    		int mid = left + (right - left) / 2;
+    		if (nums[mid] < nums[mid - 1]) {
+    			return nums[mid];
+    		} else if (nums[mid] > nums[right]) {
+    			left = mid + 1;
+    		} else {
+    			right = mid - 1;
+    		}
+    	}
+    	return nums[left];
+    }
+}
