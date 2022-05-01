9f5a1801d7555007dcfced46c5f994aca326e72d
Song X. Gao <39278329+xsgao-github@users.noreply.github.com>
2022-04-30 20:33:05 -0400

    add LeetCode 0020

---
+++ java-impl/app/src/test/java/ads/java/leetcode/LC0020ValidParenthesesTest.java
@@ -0,0 +1,21 @@
+package ads.java.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LC0020ValidParenthesesTest {
+
+	@Test
+	void testIsValid() {
+		LC0020ValidParentheses obj = new LC0020ValidParentheses();
+		assertTrue(obj.isValid("()[]{}"));
+		assertTrue(obj.isValid("([{}])"));
+		assertFalse(obj.isValid("(]"));
+		assertFalse(obj.isValid("(]"));
+		assertFalse(obj.isValid("[)"));
+		assertFalse(obj.isValid("[}"));
+		assertFalse(obj.isValid(")[]"));
+	}
+
+}
