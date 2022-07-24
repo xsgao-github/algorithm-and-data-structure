658a1be8b4019dbbf2f3671d66fc54fc06345a59
Song X. Gao <39278329+xsgao-github@users.noreply.github.com>
2022-07-24 18:33:14 -0400

    rename files

--- java-impl/app/src/test/java/ads/java/leetcode/LC0206ReverseLinkedListTest.java
+++ java-impl/app/src/test/java/ads/java/leetcode/LC0206ReverseLinkedListTest.java
@@ -7,16 +7,15 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-class LC0206ReverseLinkedList {
+class LC0206ReverseLinkedListTest {
 
 	@Test
 	void testReverseList() {
-		LC0206ReverseLinkedList rll = new LC0206ReverseLinkedList();
+		LC0206ReverseLinkedList obj = new LC0206ReverseLinkedList();
 		ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-		assertIterableEquals(Arrays.asList(new Integer[] { 5, 4, 3, 2, 1 }), rll.reverseList(head).toList());
+		assertIterableEquals(Arrays.asList(new Integer[] { 5, 4, 3, 2, 1 }), obj.reverseList(head).toList());
 
 		head = null;
-		assertNull(rll.reverseList(head));
+		assertNull(obj.reverseList(head));
 	}
-
 }
