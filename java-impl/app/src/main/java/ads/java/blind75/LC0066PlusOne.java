a14f72cc477c6d6035c580de6727707bf05bdf74
Song X. Gao <39278329+xsgao-github@users.noreply.github.com>
2022-07-24 22:29:29 -0400

    add a blind75 case

---
+++ java-impl/app/src/main/java/ads/java/blind75/LC0066PlusOne.java
@@ -0,0 +1,37 @@
+package ads.java.blind75;
+
+public class LC0066PlusOne {
+	public int[] plusOne(int[] digits) {
+		int carryover = 0;
+		// plus one
+		int idx = digits.length - 1;
+		if (digits[idx] + 1 == 10) {
+			digits[idx] = 0;
+			carryover = 1;
+		} else {
+			digits[idx] = digits[idx] + 1 + carryover;
+			carryover = 0;
+		}
+
+		// carryover
+		for (idx--; idx >= 0; idx--) {
+			if (digits[idx] + carryover == 10) {
+				digits[idx] = 0;
+				carryover = 1;
+			} else {
+				digits[idx] = digits[idx] + carryover;
+				carryover = 0;
+				break;
+			}
+		}
+
+		if (carryover == 0) {
+			return digits;
+		} else {
+			int[] ret = new int[digits.length + 1];
+			System.arraycopy(digits, 0, ret, 1, digits.length);
+			ret[0] = 1;
+			return ret;
+		}
+	}
+}
---
+++ java-impl/app/src/test/java/ads/java/blind75/LC0066PlusOneTest.java
@@ -0,0 +1,18 @@
+package ads.java.blind75;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LC0066PlusOneTest {
+
+	@Test
+	void test() {
+		LC0066PlusOne obj = new LC0066PlusOne();
+//		assertArrayEquals(new int[] { 1, 2, 4 }, obj.plusOne(new int[] { 1, 2, 3 }));
+//		assertArrayEquals(new int[] { 4, 3, 2, 2 }, obj.plusOne(new int[] { 4, 3, 2, 1 }));
+		assertArrayEquals(new int[] { 2, 0 }, obj.plusOne(new int[] { 1, 9 }));
+		assertArrayEquals(new int[] { 1, 0, 0 }, obj.plusOne(new int[] { 9, 9 }));
+	}
+
+}
